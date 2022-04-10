151ed66ed71104b5ab7f3417d2b3fa28d1af37e6
Pavan Choudhari <pavanwebdevcs5610@gmail.com>
2022-04-10 16:53:28 -0400

    Change to API BASE

--- src/components/tuiter/service/tuits-service.js
+++ src/components/tuiter/service/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://full-stack-developer--server.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE_CHANGE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 
